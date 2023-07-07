474c1cdb5de5ce62d1526676cb00dc28966bf18f
Deblak <deblak@mailo.com>
2023-07-07 15:41:00 +0200

    test branch test 4

--- app/src/main/java/fr/doranco/flash/activites/Contact.java
+++ app/src/main/java/fr/doranco/flash/activites/Contact.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import fr.doranco.flash.R;
 
 public class Contact extends AppCompatActivity {
+    //test avec Branch test
+    //comit and puch
+    // comit push merge
+    // comit in master
+    // master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
