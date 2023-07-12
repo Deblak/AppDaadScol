6d9ae385d50c90a255a4b844824b43581785861d
Deblak <deblak@mailo.com>
2023-07-12 16:03:32 +0200

    Exercice ordre de lettres

---
+++ app/src/main/java/fr/doranco/flash/ui/quiz/OdreLettreActivity.java
@@ -0,0 +1,16 @@
+package fr.doranco.flash.ui.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.doranco.flash.R;
+
+public class OdreLettreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_odre_lettre);
+    }
+}
