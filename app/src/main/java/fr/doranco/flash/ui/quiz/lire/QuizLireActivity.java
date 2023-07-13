cd6732daeb7efb32e9d3960f79c62ce999b102c6
Deblak <deblak@mailo.com>
2023-07-13 11:00:04 +0200

    test push quiz lire activity

--- app/src/main/java/fr/doranco/flash/ui/quiz/lire/QuizLireActivity.java
+++ app/src/main/java/fr/doranco/flash/ui/quiz/lire/QuizLireActivity.java
@@ -18,6 +18,7 @@ public class QuizLireActivity extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
