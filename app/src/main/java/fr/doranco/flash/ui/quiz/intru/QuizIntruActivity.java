3c2b5e668347a3776297bffc1c4274ee59e41870
Deblak <deblak@mailo.com>
2023-07-18 15:00:00 +0200

    Création quiz 'chercher intru', ajout drawables des lettres, fichier XML

---
+++ app/src/main/java/fr/doranco/flash/ui/quiz/intru/QuizIntruActivity.java
@@ -0,0 +1,16 @@
+package fr.doranco.flash.ui.quiz.intru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.doranco.flash.R;
+
+public class QuizIntruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_intru);
+    }
+}
